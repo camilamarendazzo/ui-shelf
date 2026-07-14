fcaa2c01df901c89688db86e7978a404392147b4
camilamarendazzo <camimarendazzo@gmail.com>
2026-07-13 21:47:21 -0300

    fix: adjust spacing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ function Layout() {
 
       <Header />
 
-      <main id="main" className="mx-auto w-full max-w-5xl flex-1 px-6 py-12">
+      <main id="main" className="mx-auto w-full max-w-7xl flex-1 px-6 py-12">
         <Outlet />
       </main>
 
